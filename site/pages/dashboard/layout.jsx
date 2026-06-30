eae76859ce90213fe1489be5aa1799ae653adb35
Saeed Nemati <sn.1361@gmail.com>
2026-06-30 10:46:25 +0330

    committed by script

--- site/pages/dashboard/layout.jsx
+++ site/pages/dashboard/layout.jsx
@@ -8,7 +8,7 @@ import {
     loadDashboard,
     SignOut,
 } from 'accounts'
-import { Menu } from 'dashboard'
+// import { Menu } from 'dashboard'
 
 export const onRequest = event => {
     checkLogin(event)
@@ -24,17 +24,17 @@ export default component$(({ }) => {
         <div class='flex flex-col sm:flex-row max-w-6xl mx-auto bg--100 rounded-md my-10 sm:p-2'>
             <div class='sm:w-[30%] flex flex-col sm:flex-none p-2 gap-y-2'>
                 {
-                    Menu?.map((item, index) => {
-                        return <button
-                            class={`p-2 block rounded-md ${url.pathname.includes(item.path) ? 'bg-custom-color1 text-white' : 'bg-gray-200 hover:bg-custom-color1/40 duration-300'}`}
-                            key={index + 1}
-                            onClick$={() => {
-                                window.location.href = `/dashboard${item.path}`
-                            }}
-                        >
-                            {translations[item.title]}
-                        </button>
-                    })
+                    // Menu?.map((item, index) => {
+                    //     return <button
+                    //         class={`p-2 block rounded-md ${url.pathname.includes(item.path) ? 'bg-custom-color1 text-white' : 'bg-gray-200 hover:bg-custom-color1/40 duration-300'}`}
+                    //         key={index + 1}
+                    //         onClick$={() => {
+                    //             window.location.href = `/dashboard${item.path}`
+                    //         }}
+                    //     >
+                    //         {translations[item.title]}
+                    //     </button>
+                    // })
                 }
                 <SignOut>
                     <button class='p-2 block rounded-md text-white bg-red-600 hover:bg-red-400 duration-300 w-full cursor-pointer mt-10'>

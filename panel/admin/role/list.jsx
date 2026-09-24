a100eecf596d983edddbd62f078b77ee4f5eee40
Saeed Nemati <sn.1361@gmail.com>
2026-09-24 09:08:55 +0330

    committed by script

--- panel/admin/role/list.jsx
+++ panel/admin/role/list.jsx
@@ -1,10 +1,6 @@
 import { List } from 'list'
 import form from './form'
-import headers from './headers'
-import row from './row'
 
 export default <List
     create={form}
-    headers={headers}
-    row={row}
 />
--- panel/admin/user/list.jsx
+++ panel/admin/user/list.jsx
@@ -1,17 +1,9 @@
 import { List } from 'list'
 import form from './form'
-import headers from './headers'
-import itemActions from './itemActions'
-import listActions from './listActions'
-import row from './row'
 import sorts from './sorts'
 
 export default <List
     create={form}
-    itemActions={itemActions}
     hasDelete
-    headers={headers}
-    listActions={listActions}
-    row={row}
     sorts={sorts}
 />
